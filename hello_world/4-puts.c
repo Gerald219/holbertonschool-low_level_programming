6d604b68dec9671e820f5c493d373aad876f6d09
GeraldMulero <gdmasm@gmail.com>
2025-02-20 10:48:48 -0400

    fixing betty descriptions

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point of the program
  *
- * Return: always 0 (success)
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * using the `puts` function.
+ *
+ * Return: Always 0 (Success)
  */
 int main (void)
 {
