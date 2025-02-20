3e451397c2a095b2da230569719075960fc61f4d
GeraldMulero <gdmasm@gmail.com>
2025-02-20 11:53:48 -0400

    uses printf function, fixed grammar

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,12 +3,13 @@
 
 /**
  * main - entry point of program 
- * Description: prints with proper grammar, but the outcome is a piece of art,
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * using the 'printf' function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, butthe outcome is a piece of art,");\n
+	printf("\"with proper grammar, butthe outcome is a piece of art,\n");
 		return (0);
 }
