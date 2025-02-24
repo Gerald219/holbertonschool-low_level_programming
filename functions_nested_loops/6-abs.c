2cb44ff14f0c9f343a1042735e651fe0266738ea
GeraldMulero <gdmasm@gmail.com>
2025-02-23 23:48:24 -0400

    function _abs presents the value of integer(a whole number), but it shows always a positive outcome

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: The integer to be checked.
+ *
+ * Return: The absolute value of n. 
+ */
+
 int _abs(int n)
 {
 	if (n < 0)
