233b85cb690c729c2b602b876b7d7530d8433482
GeraldMulero <gdmasm@gmail.com>
2025-02-24 01:28:37 -0400

    Uses the add function to present to values together which adds up to 98

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,9 +4,10 @@
  * add - Adds two integers and returns the result
  * @a: First integer
  * @b: Second integer
+ *
  * Return: Sum of a and b
  */
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
