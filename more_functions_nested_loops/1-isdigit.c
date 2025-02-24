4ebc79647da35019ec13886f8af8d22173833db2
GeraldMulero <gdmasm@gmail.com>
2025-02-24 14:18:21 -0400

    The goal with this function is to identify if c value is a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - Checks if a character is a digit (0-9).
- * @c: The character to check. 
+ * @c: The character to check.
  *
  * Description: The function is to process an integer that is representing
  * an ASCII character and checks if it's a digit (0-9).
@@ -15,4 +15,4 @@ int _isdigit(int c)
 		return (1);
 	else
 		return (0);
-} 
+}
