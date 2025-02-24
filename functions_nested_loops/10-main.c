c17e7df199588784b33fb3f83c05eadb3dcb3d0b
GeraldMulero <gdmasm@gmail.com>
2025-02-24 01:24:35 -0400

    Uses the add function to present to values together which adds up to 98

---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Tests the add function
+ * Return: Always 0
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
