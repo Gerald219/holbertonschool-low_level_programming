9836e8fd0a2145d0f687be57bfe16c9ea59f3ce9
GeraldMulero <gdmasm@gmail.com>
2025-02-24 00:53:03 -0400

    The function is to deliver the last digit and its always positive

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
+ *
+ * Return: The last digit of n
+ */
 int print_last_digit(int n)
 {
 	int last_digit;
