88c20f2bb3cd6a2f2b204368ac4f8f57d196ef59
GeraldMulero <gdmasm@gmail.com>
2025-02-24 00:45:00 -0400

    The function is to deliver the last digit and its always positive

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
     int last_digit;
 
-    last_digit = n % 10;  /* Get last digit */
+    last_digit = n % 10;
     if (last_digit < 0)
-        last_digit = -last_digit;  /* Convert negative last digit to positive */
+        last_digit = -last_digit;
 
-    _putchar(last_digit + '0');  /* Print the last digit */
-    return (last_digit);  /* Return the last digit */
+    _putchar(last_digit + '0');
+    return (last_digit);
 }
