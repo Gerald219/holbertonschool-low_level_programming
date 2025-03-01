1e3c34b25702b0b44d422bc33f4a782d5601b867
GeraldMulero <gdmasm@gmail.com>
2025-03-01 19:55:15 -0400

    The function reverses only half of the string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * rev_string - Prints a reversed string.
- * @s: pointer to the string.
- *
+ * rev_string - Reverses a string in place
+ * @s: Pointer to the string
  */
 void rev_string(char *s)
 {
-    int length = 0, start = 0;
+	int length = 0, start = 0;
+	char temp;
 
-    while (s[length] != '\0')
-        length++;
+	while (s[length] != '\0') /* Find string length */
+		length++;
 
-    length--;
+	length--; /* Adjust for last character */
 
-    while (start < length)
-    {
-        char temp = s[start];
-        s[start] = s[length];
-        s[length] = temp;
-        start++;
-        length--;
-    }
+	while (start < length) /* Swap characters */
+	{
+		temp = s[start];
+		s[start] = s[length];
+		s[length] = temp;
+		start++;
+		length--;
+	}
 }
 	
