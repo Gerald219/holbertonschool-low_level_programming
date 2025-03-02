e6d5e0a2333096cb472f9ad47f76df3d596c8129
GeraldMulero <gdmasm@gmail.com>
2025-03-01 23:10:19 -0400

    the functions prints the second half of a string, calculating its length

---
+++ pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the puts_half function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str = "0123456789";
+
+	puts_half(str);
+
+	return (0);
+}
