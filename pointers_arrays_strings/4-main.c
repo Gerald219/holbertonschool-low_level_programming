2bd69992416deb921b9cf98c174f4923fe97c5c2
GeraldMulero <gdmasm@gmail.com>
2025-03-02 23:51:36 -0400

    The function of this is to swap the order of an array from start to end

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,16 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the print_rev function
+ * print_array - Prints an array of integers.
+ * @a: The array.
+ * @n: The number of elements.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - Tests reverse_array function.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	int n = sizeof(a) / sizeof(a[0]);
 
-	str = "I do not fear computers. I fear the lack of them - Isaac asimov";
-	print_rev(str);
+	print_array(a, n);
+	reverse_array(a, n);
+	print_array(a, n);
 
 	return (0);
 }
