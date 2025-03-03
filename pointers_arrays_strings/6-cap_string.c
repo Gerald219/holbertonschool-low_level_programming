b93e84ca39b6c7382a39cd3d11c27c2671196d43
GeraldMulero <gdmasm@gmail.com>
2025-03-02 23:29:06 -0400

    this function capitalizes the first letter of each word in a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,27 @@
 #include "main.h"
-#include <string.h> /* For strchr */
 
-/**
- * cap_string - Capitalizes all words of a string.
- * @str: The input string.
- *
- * Return: The pointer to the modified string.
- */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int cap_next = 1; 
+	int i, j;
+	int cap_next = 1;
 	char separators[] = " \t\n,;.!?\"(){}";
 
+	i = 0;
 	while (str[i] != '\0')
 	{
 		if (cap_next && (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
-		cap_next = (strchr(separators, str[i]) != NULL);
+
+		cap_next = 0;
+
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				cap_next = 1;
+				break;
+			}
+		}
 		i++;
 	}
 	return (str);
