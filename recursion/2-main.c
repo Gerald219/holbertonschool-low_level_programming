58b95929e1afcdbc9b328595e7a4f26f9da5e351
GeraldMulero <gdmasm@gmail.com>
2025-03-06 13:47:07 -0400

    The function _strlen_recursion counts the characters through recursion, counting while moving forward one step at a time

---
+++ recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point test _strlen_recursion function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin coleman");
+	printf("%d\n", n);
+	return (0);
+}
