4204ee48331053982de191a075ee90a166242e48
GeraldMulero <gdmasm@gmail.com>
2025-03-05 22:46:55 -0400

    The recursion function calls each letter one at a time through a loop, and uses _putchar to print it, until none are left

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,42 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * simple_print_buffer - Prints buffer to hexadecimal format.
- * @buffer: Address of memory to print.
- * @size: Size of memory to print.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
-		printf("0x%02x", buffer[1]);
-				}
-				printf("\n");
-				}
-/**
- * main - Tests the _memset function.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char buffer[98] = {0x00};
-
-printf("Before memset:\n");
-simple_print_buffer(buffer, 98);
-
-_memset(buffer, 0x01, 95);
-
-printf("After memset:\n");
-simple_print_buffer(buffer, 98);
-
-return (0);
+	_puts_recursion("Puts with recursion");
+	_putchar('\n');
+	return (0);
 }
