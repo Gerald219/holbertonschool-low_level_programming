5df7c48dde6085b1569a3bae4af0bcd1129b2e03
GeraldMulero <gdmasm@gmail.com>
2025-03-08 13:54:31 -0400

    The function of _sqrt_recursion checks numbers starting from 0, increasing one by one, until it finds a number that when squared equals n, or returns -1 if no perfect square exists.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * find_sqrt - Helper function to find square root.
+ * @n: The number to find the square root of.
+ * @guess: The current number being tested as a square root.
+ *
+ * Return: The natura square root, or -1 if none exists.
+ */
+int find_sqrt(int n, int guess)
+{
+	if (guess * guess > n) return (-1);
+	if (guess * guess == n) return (guess);
+	return (find_sqrt(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - Return the natural square root of a number.
+ * @n: The number to find the square rot of.
+ *
+ * Return: The natural square root, or -1 if no natural square root exists.
+ */
+int _sqrt_recursion(int n)
+{
+	return (n < 0 ? -1 : find_sqrt(n, 0));
+}
