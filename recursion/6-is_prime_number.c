18507214ef227dcff5b02b3e060e4494600e7998
GeraldMulero <gdmasm@gmail.com>
2025-03-08 14:26:53 -0400

    The function in this incursion is to find a prime number, the selected number is divided each time by a number, if it can split the value in half it returns 0, if it can only bedivided by one and its value it returns 1

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * check_prime - Recursively checks if a number is prime.
+ * @n: The number to check.
+ * @div: the divisor to test.
+ *
+ * Return: 1 if prime, 0 if not.
+ */
+int check_prime(int n, int div)
+{
+	if (n <= 1)
+		return (0);
+	if (div == 1)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (check_prime(n, div - 1));
+}
+
+/**
+ * is_prime_number - Checks if an integer is a prime number.
+ * @n: The number to check.
+ *
+ * Return: 1 if prime, 0 otherwise.
+ */
+int is_prime_number (int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, n / 2));
+}
