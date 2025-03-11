757255538602068d2fc54ff52a2723195fc5da16
GeraldMulero <gdmasm@gmail.com>
2025-03-10 22:53:01 -0400

    the function multiples numbers placed on the terminal, and after prints the result.

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers from command-line arguments.
+ * @argc: Argument count.
+ * @argv: Argument vector (arrat of strings).
+ *
+ * Return: 0 if successful, 1 if incorrect number of arguments.
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+
+    if (argc != 3)  /* Must have exactly 2 arguments */
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    result = num1 * num2;
+
+    printf("%d\n", result);
+    return (0);
+}
