ffce55e6f6e2cc4d441f3b8c6ad60b753816d902
GeraldMulero <gdmasm@gmail.com>
2025-03-13 14:29:52 -0400

    string_nconcat joins two strings by taking s1 completly and only n amount of characters from s2 and storing it in new memory

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Number of bytes from s2 to concatenate.
+ *
+ * Return: Pointer to newly allocated memory containing s1 + first n of s2.
+ * NULL if allocating fails.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	if (n > len2)
+		n = len2;
+	result = malloc(sizeof(char) * (len1 + n +1));
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		result[i] = s2[j];
+	result[i] = '\0';
+
+	return (result);
+}
