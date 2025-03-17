4c9fc204beef129fad4d625cd7971cf381081724
GeraldMulero <gdmasm@gmail.com>
2025-03-16 23:28:02 -0400

    betty style error fixed excess of 85 characters

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * create_array - Creates an array of chars and initializes it with a spesific char.
+ * create_array - Creates an array of chars and initializes
+ * it with a spesific char.
  * @size: The size of the array.
  * @c: The character to initialize the array with.
  *
