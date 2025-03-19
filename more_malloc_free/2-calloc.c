ce0a88e3b9d79ce2c547d0ca7fd494d605105550
GeraldMulero <gdmasm@gmail.com>
2025-03-18 22:51:07 -0400

    We use _calloc to allocate 98 empty slots, these start at 0, we use the function strcpy to print in them in spesific spots, simple_print_buffer prints it on screen and free() deletes after use

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,25 +1,27 @@
-#include "main.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array and initializes it to zero.
- * @nmemb: Number of elements.
- * @size: Size of each element.
+ * _calloc - Allocates memory for an array and intializes to zero
+ * @nmemb: Number of elements
+ * @size: Size of each element
  *
- * Return: Pointer to allocated memory, or NULL if allocation fails.
+ * Return: Pointer to allocated memory, or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int total_size;
+	char *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (!ptr)
 		return (NULL);
 
-	memset(ptr, 0, nmemb * size);
-
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
 	return (ptr);
 }
--- more_malloc_free/2-main.c
+++ more_malloc_free/2-main.c
@@ -1,37 +1,46 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hex
- * @buffer: Memory address to print
- * @size: Memory size to print
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+	unsigned int i; 
 
 	i = 0;
+
 	while (i < size)
 	{
-		if (i < size)
+		if (i % 10)
+		{
 			printf(" ");
+		}
 		if (!(i % 10) && i)
-			printf("0x%02x", buffer[i]);
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
 		i++;
 	}
 	printf("\n");
 }
 /**
- * main -Tests _calloc function
+ * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int main (void)
 {
 	char *a;
 
-	a = _calloc(98, sizeof(char));
+	a = _calloc(98, sizeof(char)); 
 	strcpy(a, "Best");
 	strcpy(a + 4, " school! :)\n");
 	a[97] = '!';
