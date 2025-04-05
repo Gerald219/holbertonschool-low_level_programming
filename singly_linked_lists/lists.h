f88f1892445eb90eb83de8d7ac49ceec092a69e0
GeraldMulero <gdmasm@gmail.com>
2025-04-05 00:11:48 -0400

    Created the tester file, where it calls add_node(), creating a linked list of names, from most recent to oldest. Len stores the length of each node and printf using %u prints the number and %s the name.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -9,7 +9,6 @@
  * @str: a malloc string, can represent (NULL)
  * @len: the length of the string
  * @next: pointer to the next node in the list, or NULL if gone
- * struct list_s: info about one item in the list
  *
  * Description: node structure for a singly linked list
  */
@@ -23,5 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
