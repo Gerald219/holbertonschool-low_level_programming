74cf8ee3b898be6f13c7b0d6af8e2fa35410863e
GeraldMulero <gdmasm@gmail.com>
2025-04-06 16:51:37 -0400

    Removed trailing space

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function for the operator
@@ -9,23 +10,23 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
 
-    int i = 0;
+int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (*(s) == *(ops[i].op))
-            return (ops[i].f);
-        i++;
-    }
+while (ops[i].op != NULL)
+{
+	if (strcmp(s, ops[i].op) == 0)
+		return (ops[i].f);
+	i++;
+}
 
-    return (NULL);
+return (NULL);
 }
