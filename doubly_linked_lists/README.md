5d8d7348192cf0531a93bad4071ec1944306afa4
GeraldMulero <gdmasm@gmail.com>
2025-04-10 17:57:16 -0400

    The function goes through each node in the list, and printsthe number it holds, and tells you how many nodes it found in total.

---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This project is about building functions, that can print, count, add, insert delete and sum nodes in both direction, to assure everything links up clean without leaks or crashes.
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: int data
+ * @prev: pointer to previous node
+ * @next: pointer to the next node
+ *
+ * Description: node structure for doubly linked list
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
