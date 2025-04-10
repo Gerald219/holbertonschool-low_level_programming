ce018d7a3dd105b63d7525a65e52a2748bae278b
GeraldMulero <gdmasm@gmail.com>
2025-04-09 20:49:07 -0400

    Last attempt

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,44 +2,89 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - prints a char
+ * @args: va_list
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: va_list
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: va_list
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
+
 /**
  * print_all - prints anything
- * @format: a list of types of arguments passed to the function
- *
+ * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str;
 	va_list args;
-	char *sep = " ";
+	unsigned int i = 0, j;
+	char *separator = "";
+
+	typedef struct fmt
+	{
+		char symbol;
+		void (*f)(va_list);
+	} fmt_t;
+
+	fmt_t formats[] = {
+		{'c', print_char}, {'i', print_int},
+		{'f', print_float}, {'s', print_string},
+		{0, NULL}
+	};
 
 	va_start(args, format);
 
-		while (format && format[i])
+	while (format && format[i])
+	{
+		j = 0;
+		while (formats[j].symbol)
 		{
-			if (format[i] == 'c' || format[i] == 'i' ||
-			format[i] == 'f' || format[i] == 's')
+			if (format[i] == formats[j].symbol)
 			{
-				printf("%s", sep);
-				if (format[i] == 'c')
-					printf("%c", va_arg(args, int));
-				if (format[i] == 'i')
-					printf("%d", va_arg(args, int));
-				if (format[i] == 'f')
-					printf("%f", va_arg(args, double));
-				if (format[i] == 's')
-				{
-					str = va_arg(args, char *);
-					if (str)
-						printf("%s", str);
-					else
-						printf("(nil)");
-				}
-				sep = ", ";
+				printf("%s", separator);
+				formats[j].f(args);
+				separator = ", ";
 			}
-			i++;
+			j++;
 		}
+		i++;
+	}
 	va_end(args);
 	printf("\n");
 }
