9ec187061eda8dc76746b699c58366de7e70f0f8
GeraldMulero <gdmasm@gmail.com>
2025-04-10 18:02:41 -0400

    Added missing l in one line description comment for print_dlistint function

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_distint - prints all elements in a body doubly linked list
+ * print_dlistint - prints all elements in a body doubly linked list
  * @h: pointer to the head of the list
  *
  * Return: number of nodes
