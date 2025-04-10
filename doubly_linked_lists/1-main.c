946a38a2e653f3785d94185bd5453e77244c7b3b
GeraldMulero <gdmasm@gmail.com>
2025-04-10 18:28:31 -0400

    The function goes through the list of node at a time, counts them as it goes, then stops when there is one left

---
+++ doubly_linked_lists/1-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - test the function that adds nodes at the beginning
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	add_dnodeint(&head, 4);
+	add_dnodeint(&head, 98);
+	add_dnodeint(&head, 402);
+	add_dnodeint(&head, 1024);
+
+	print_dlistint(head);
+
+	return (EXIT_SUCCESS);
+}
