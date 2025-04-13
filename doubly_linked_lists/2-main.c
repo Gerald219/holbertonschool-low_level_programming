e92503afd0f81963d9e4942c1de1fc7791c02aa0
GeraldMulero <gdmasm@gmail.com>
2025-04-13 19:34:10 -0400

    The function add_dnodeint uses malloc to create a new node, checks with if, if it worked, connected it to the old list using next and prev pointers, and updates the list's start in main.c, then 0-print_dlistint shows each add worked by printing the full list

---
+++ doubly_linked_lists/2-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - test the function add_dnodeint to add nodes at the beginning
+ *
+ * Return: Always Exit_success
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	add_dnodeint(&head, 4);
+	add_dnodeint(&head, 98);
+	add_dnodeint(&head, 402);
+	add_dnodeint(&head, 1024);
+
+	print_dlistint(head);
+
+	return (EXIT_SUCCESS);
+}
