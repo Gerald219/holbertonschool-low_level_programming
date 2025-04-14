32e01bca2c2e2fc6870642e8688ca34b90dbd4a7
GeraldMulero <gdmasm@gmail.com>
2025-04-13 23:24:45 -0400

    a list is built with add_dnodeint_end, then finds index 4, and using malloc creates a new node to place 4096 in between nodes 4 and 98 by linking new->prev to 4 and new->next to 98, updating it to point now with the new node

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - puts a new numbers into the list at a spesific spot
+ * @h: address of the firs node in the list
+ * @idx: the spot(index) where the number should be placed
+ * @n: the number to be inserted
+ *
+ * Return: the new node address, or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *current = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL || current->next == NULL)
+		return add_dnodeint_end(h, n);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = current;
+	new->next = current->next;
+
+	current->next->prev = new;
+	current->next = new;
+
+	return (new);
+}
+
